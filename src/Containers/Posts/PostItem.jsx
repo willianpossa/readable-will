1602fd32a09bc2c506ee13b1429c176778007916
Willian Stroher Possa <willian.wejam@gmail.com>
2019-04-04 00:46:05 -0300

    Post card style

--- src/Containers/Posts/PostItem.jsx
+++ src/Containers/Posts/PostItem.jsx
@@ -1,13 +1,55 @@
 import React, { Component } from 'react'
 
-class ItemPost extends Component {
+import { 
+    ArticleWrapper, 
+    ArticleInfo, 
+    Title, 
+    GeneralsInfo, 
+    Date, 
+    Author, 
+    ActionsWrapper, 
+    Category, 
+    Action, 
+    Comments, 
+    LikeWrapper, 
+    Like,
+    Arrow
+} from './PostItemStyle'
+
+class PostItem extends Component {
 
     render() {
+        const { title, author, date, comments, category, votes } = this.props
 
         return (
-            <div>Post Item</div>
+            <ArticleWrapper>
+                <ArticleInfo>
+                    <Title>{ title }</Title>
+                    <GeneralsInfo>
+                        <Author>{ author }</Author> - <Date>{ date }</Date>
+                        <Comments>
+                            <i className="icon-comments"></i>
+                            { comments } comentário{ comments > 1 ? 's' : '' }
+                        </Comments>
+                    </GeneralsInfo>
+                    <ActionsWrapper>
+                        <Category>{ category }</Category>
+                        <Action className="blue">Editar</Action>
+                        <Action className="red">Deletar</Action>
+                    </ActionsWrapper>
+                </ArticleInfo>
+                <LikeWrapper>
+                    <Arrow>
+                        <i className="icon-up-arrow"></i>
+                    </Arrow>
+                    <Like>{ votes }</Like>
+                    <Arrow>
+                        <i className="icon-down-arrow"></i>
+                    </Arrow>
+                </LikeWrapper>
+            </ArticleWrapper>
         )
     }
 }
 
-export default ItemPost
+export default PostItem
--- src/Containers/Posts/Posts.jsx
+++ src/Containers/Posts/Posts.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import PostItem from './PostItem';
 
 class ListPosts extends Component {
 
     render() {
 
         return (
-            <div>Posts List</div>
+            <div>
+                <PostItem 
+                    title="Udacity é o melhor lugar para se aprender React"
+                    author="Willian Possa"
+                    date="09/05/2019 18:43"
+                    comments={ 2 }
+                    category="react"
+                    votes={ 10 }
+                />
+            </div>
         )
     }
 }
